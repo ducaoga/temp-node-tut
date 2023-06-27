248c6e357f08ed82fe5ece97a364633b1e3050ab
Gail Ducao <gail.s.ducao@accenture.com>
2023-06-27 17:26:16 +0800

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,11 @@
+//commonjs - every file is  module (by default)
+//modules - encapsulated code (only share minimum)
+
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavor');
+require('./7-mind-granade');
+
+
+// sayHi('susan');
+// sayHi(names.john);
---
+++ intro.js.js
@@ -0,0 +1,10 @@
+const amount = 12;
+
+if(amount >10){
+    console.log("big");
+}else{
+    console.log("small");
+}
+
+
+console.log("hey its my node app");
